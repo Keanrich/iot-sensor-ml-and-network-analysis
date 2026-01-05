5185bb86b1bf66313d18a2e3bbc611487bc5f93f
Keanrich <cordana2006@gmail.com>
2026-01-05 22:31:39 +0700

    Clean up unused DHT sensor code in main.cpp
    Removed commented-out DHT sensor code and related setup.

--- src/main.cpp
+++ src/main.cpp
@@ -210,34 +210,3 @@ void loop() {
   }
   delay(20000);
 }
-
-
-
-// #include <Adafruit_Sensor.h>
-// #include <DHT.h>
-// #include <DHT_U.h>
-
-// #define DHTPIN D5
-// #define DHTTYPE DHT22
-// DHT dht(DHTPIN, DHTTYPE);
-
-// void setup() {
-//   Serial.begin(115200);
-//   dht.begin();
-// }
-
-// void loop() {
-//   float h = dht.readHumidity();
-//   float t = dht.readTemperature();
-
-//   if (isnan(h) || isnan(t)) {
-//     Serial.println("Failed to read from DHT22!");
-//   } else {
-//     Serial.print("Humidity: ");
-//     Serial.print(h);
-//     Serial.print(" %  Temperature: ");
-//     Serial.print(t);
-//     Serial.println(" °C");
-//   }
-//   delay(2000);
-// }
